04c39062a32483b9fdb4cefc0b4fca2a93c370eb
Patrick Fairbank <patfair@gmail.com>
2023-11-18 13:16:32 -0800

    Bind only to the 10.0.100.x address.

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"regexp"
 )
 
 const port = 8081
@@ -14,7 +16,38 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	ipAddress, err := getVlan100IpAddress()
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+
+	listenAddress := fmt.Sprintf("%s:%d", ipAddress, port)
 	http.HandleFunc("/", handler)
-	fmt.Printf("Server listening on :%d\n", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	fmt.Printf("Server listening on %s\n", listenAddress)
+	if err := http.ListenAndServe(listenAddress, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// getVlan100IpAddress returns the IP address of the first interface that has an IP address on the 10.0.100.x VLAN.
+func getVlan100IpAddress() (string, error) {
+	ipRe := regexp.MustCompile("^(10\\.0\\.100\\.\\d+)")
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			address := addr.String()
+			matches := ipRe.FindStringSubmatch(address)
+			if len(matches) != 0 {
+				return matches[1], nil
+			}
+		}
+	}
+	return "", fmt.Errorf("no IP address found on VLAN 100 (i.e. matching %v)", ipRe)
 }
