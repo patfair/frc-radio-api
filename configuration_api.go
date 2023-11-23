97c6ec1446545d8895333f04ca1c2ca50c46fb44
Patrick Fairbank <patfair@gmail.com>
2023-11-22 16:25:25 -0800

    Validate parameters passed to the configuration API.

--- configuration_api.go
+++ configuration_api.go
@@ -20,16 +20,25 @@ type stationConfiguration struct {
 	WpaKey string `json:"wpaKey"`
 }
 
+var validLinksysChannels = []int{36, 40, 44, 48, 149, 153, 157, 161, 165}
+
 // configurationHandler receives a JSON request to configure the access point and adds it to the asynchronous queue.
 func (web *web) configurationHandler(w http.ResponseWriter, r *http.Request) {
 	var request configurationRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		handleWebErr(w, err)
+		errorMessage := "Error: invalid JSON: " + err.Error()
+		log.Println(errorMessage)
+		http.Error(w, errorMessage, http.StatusBadRequest)
 		return
 	}
-
 	if reflect.DeepEqual(request, configurationRequest{}) {
 		errorMessage := "Error: received empty configuration request"
+		log.Println(errorMessage)
+		http.Error(w, errorMessage, http.StatusBadRequest)
+		return
+	}
+	if err := request.validate(web.accessPoint.hardwareType); err != nil {
+		errorMessage := "Error: " + err.Error()
 		log.Println(errorMessage)
 		http.Error(w, errorMessage, http.StatusBadRequest)
 		return
@@ -38,4 +47,52 @@ func (web *web) configurationHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received configuration request: %+v", request)
 	web.accessPoint.enqueueConfigurationRequest(request)
 	_, _ = fmt.Fprintln(w, "New configuration received and will be applied asynchronously.")
+}
+
+// validate checks that all parameters within the configuration request have valid values.
+func (request configurationRequest) validate(accessPointType accessPointType) error {
+	if request.Channel != 0 {
+		// Validate channel number.
+		valid := false
+		switch accessPointType {
+		case typeLinksys:
+			for _, channel := range validLinksysChannels {
+				if request.Channel == channel {
+					valid = true
+					break
+				}
+			}
+		case typeVividHosting:
+			x := (request.Channel - 5) / 8
+			y := (request.Channel - 5) % 8
+			valid = y == 0 && x >= 0 && x <= 28
+		}
+		if !valid {
+			return fmt.Errorf("invalid channel for %s: %d", accessPointType.String(), request.Channel)
+		}
+	}
+
+	// Validate station configurations.
+	for stationName, stationConfiguration := range request.StationConfigurations {
+		stationNameValid := false
+		for name := red1; name < stationCount; name++ {
+			if stationName == name.String() {
+				stationNameValid = true
+				break
+			}
+		}
+		if !stationNameValid {
+			return fmt.Errorf("invalid station: %s", stationName)
+		}
+		if stationConfiguration.Ssid == "" {
+			return fmt.Errorf("SSID for station %s cannot be blank", stationName)
+		}
+		if len(stationConfiguration.WpaKey) < 8 {
+			return fmt.Errorf(
+				"invalid WPA key length for station %s: %d", stationName, len(stationConfiguration.WpaKey),
+			)
+		}
+	}
+
+	return nil
 }
--- configuration_api_test.go
+++ configuration_api_test.go
@@ -17,7 +17,7 @@ func TestWeb_configurationHandler(t *testing.T) {
 
 	// Request to configure a single team.
 	recorder = web.postHttpResponse(
-		"/configuration", `{"stationConfigurations": {"blue1": {"ssid": "254", "wpaKey": "foo"}}}`,
+		"/configuration", `{"stationConfigurations": {"blue1": {"ssid": "254", "wpaKey": "12345678"}}}`,
 	)
 	assert.Equal(t, 200, recorder.Code)
 	assert.Contains(t, recorder.Body.String(), "configuration received")
@@ -25,7 +25,7 @@ func TestWeb_configurationHandler(t *testing.T) {
 		request := <-ap.configurationRequestChannel
 		assert.Equal(t, 0, request.Channel)
 		assert.Equal(t, 1, len(request.StationConfigurations))
-		assert.Equal(t, stationConfiguration{Ssid: "254", WpaKey: "foo"}, request.StationConfigurations["blue1"])
+		assert.Equal(t, stationConfiguration{Ssid: "254", WpaKey: "12345678"}, request.StationConfigurations["blue1"])
 	}
 
 	// Request to configure everything.
@@ -33,7 +33,7 @@ func TestWeb_configurationHandler(t *testing.T) {
 		"/configuration",
 		`
 		{
-			"channel": 11,
+			"channel": 149,
 			"stationConfigurations": {
 				"red1": {"ssid": "9991", "wpaKey": "11111111"},
 				"red2": {"ssid": "9992", "wpaKey": "22222222"},
@@ -49,7 +49,7 @@ func TestWeb_configurationHandler(t *testing.T) {
 	assert.Contains(t, recorder.Body.String(), "configuration received")
 	if assert.Equal(t, 1, len(ap.configurationRequestChannel)) {
 		request := <-ap.configurationRequestChannel
-		assert.Equal(t, 11, request.Channel)
+		assert.Equal(t, 149, request.Channel)
 		assert.Equal(t, 6, len(request.StationConfigurations))
 		assert.Equal(t, stationConfiguration{Ssid: "9991", WpaKey: "11111111"}, request.StationConfigurations["red1"])
 		assert.Equal(t, stationConfiguration{Ssid: "9992", WpaKey: "22222222"}, request.StationConfigurations["red2"])
@@ -58,4 +58,51 @@ func TestWeb_configurationHandler(t *testing.T) {
 		assert.Equal(t, stationConfiguration{Ssid: "9995", WpaKey: "55555555"}, request.StationConfigurations["blue2"])
 		assert.Equal(t, stationConfiguration{Ssid: "9996", WpaKey: "66666666"}, request.StationConfigurations["blue3"])
 	}
+}
+
+func TestWeb_configurationHandlerInvalidInput(t *testing.T) {
+	ap := newAccessPoint()
+	web := newWeb(ap)
+
+	// Invalid JSON.
+	recorder := web.postHttpResponse("/configuration", "not JSON")
+	assert.Equal(t, 400, recorder.Code)
+	assert.Contains(t, recorder.Body.String(), "invalid JSON")
+	assert.Equal(t, 0, len(ap.configurationRequestChannel))
+
+	// Invalid 5GHz channel.
+	ap.hardwareType = typeLinksys
+	recorder = web.postHttpResponse("/configuration", `{"channel": 5}`)
+	assert.Equal(t, 400, recorder.Code)
+	assert.Contains(t, recorder.Body.String(), "invalid channel for typeLinksys: 5")
+	assert.Equal(t, 0, len(ap.configurationRequestChannel))
+
+	// Invalid 6GHz channel.
+	ap.hardwareType = typeVividHosting
+	recorder = web.postHttpResponse("/configuration", `{"channel": 36}`)
+	assert.Equal(t, 400, recorder.Code)
+	assert.Contains(t, recorder.Body.String(), "invalid channel for typeVividHosting: 36")
+	assert.Equal(t, 0, len(ap.configurationRequestChannel))
+
+	// Invalid station.
+	recorder = web.postHttpResponse(
+		"/configuration", `{"stationConfigurations": {"red4": {"ssid": "254", "wpaKey": "12345678"}}}`,
+	)
+	assert.Equal(t, 400, recorder.Code)
+	assert.Contains(t, recorder.Body.String(), "invalid station: red4")
+
+	// Blank SSID.
+	recorder = web.postHttpResponse(
+		"/configuration", `{"stationConfigurations": {"blue1": {"ssid": "", "wpaKey": "12345678"}}}`,
+	)
+	assert.Equal(t, 400, recorder.Code)
+	assert.Contains(t, recorder.Body.String(), "SSID for station blue1 cannot be blank")
+
+	// Too-short WPA key.
+	recorder = web.postHttpResponse(
+		"/configuration", `{"stationConfigurations": {"blue1": {"ssid": "254", "wpaKey": "1234567"}}}`,
+	)
+	assert.Equal(t, 400, recorder.Code)
+	assert.Contains(t, recorder.Body.String(), "invalid WPA key length for station blue1: 7")
+	assert.Equal(t, 0, len(ap.configurationRequestChannel))
 }
